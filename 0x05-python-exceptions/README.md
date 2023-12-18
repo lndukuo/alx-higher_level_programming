3a5c59d99fa408f48026f4b84d328906dd2a40d8
lndukuo <leonardndukuo@gmail.com>
2023-12-18 17:04:31 +0300

    task 0

---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+this is a readme file for the exceptions on python project
---
+++ 0x05-python-exceptions/push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git add .
+
+read -p "Enter your commit message: " commit_message
+git commit -m "$commit_message"
+
+git push origin HEAD
