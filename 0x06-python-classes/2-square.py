3a64c4a56429306c682cc5b7f14d4de7880f5f82
lndukuo <leonardndukuo@gmail.com>
2023-12-19 13:53:26 +0300

    forgot to make file executable almost made me tear my haaaaaaarrghh

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-
-"""Define class Square."""
+""" defines a square """
 
 
 class Square:
-    """Rep a square."""
+    """ square with private instance attribute size """
 
     def __init__(self, size=0):
-        """Init a new square.
-        args:
-            size (init): size of the new square."""
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        """
+        Args:
+            size: size of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
