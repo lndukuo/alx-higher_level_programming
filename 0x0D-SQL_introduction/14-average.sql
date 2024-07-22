1a91029d13c6a93892347938cd4b6a94ff0ad416
lndukuo <leonardndukuo@gmail.com>
2024-07-22 16:09:10 +0300

    correction

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- Computes avarage score of all records in the second_table
-SELECT AVG(`score`) AS `avarage`
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
 FROM `second_table`;
