7a2758a848972a373f50ee81899e103526b6c80c
lndukuo <leonardndukuo@gmail.com>
2024-07-29 12:47:37 +0300

    task5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a POST request.
+# Bash scripts that sends a POST request to a given URL.
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
